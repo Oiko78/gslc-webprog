156dd388b3777fb53f89f5a99e630b1262414659
Oliver Chico <oliverchiko2002@gmail.com>
2022-11-06 12:23:17 +0700

    add partial components

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+  <p class="footer__copyright">
+    &copy; 2021 Baktify, Inc. All rights reserved.
+  </p>
+</footer>
---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,19 @@
+<header class="header">
+  <a class="header__logo button-contained" href="/">
+    <img src="" alt="">
+  </a>
+
+  <nav class="header__links">
+    <a class="button button-text" href="#">About Us</a>
+    <a class="button button-text" href="/products">Products</a>
+  </nav>
+
+  <div class="header__cta">
+    <a class="button button-text" href="#">Sign In</a>
+    <a class="button button-contained" href="#">Sign Up</a>
+    <button class="button-text hidden" type="button">Cart</button>
+    <button class="button-contained hidden" type="button">
+      <img class="button__icon" src="" alt=".">
+    </button>
+  </div>
+</header>
